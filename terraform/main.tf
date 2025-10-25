e1e0c6a368f1bc8a38ae4e26d48ea5047facb9ae
archana-kong <archana.km@konghq.com>
2025-10-25 12:54:47 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     konnect = {
       source  = "kong/konnect"
-      version = "2.4.0"
+      version = "3.3.0"
     }
   }
 }
@@ -24,18 +24,6 @@ resource "konnect_gateway_control_plane" "kongair_internal_cp" {
     generated_by =  "terraform"
   }
   name = "KongAir_Internal"
-}
-resource "konnect_gateway_control_plane" "kongair_external_cp" {
-  name = "KongAir_External"
-  auth_type     = "pki_client_certs"
-  cloud_gateway = false
-  cluster_type  = "CLUSTER_TYPE_CONTROL_PLANE"
-  description   = "CP for KongAir External API Configurations"
-  labels = {
-    env = var.environment
-    team = "kong-air-external"
-    generated_by =  "terraform"
-  }
 }
 resource "konnect_gateway_control_plane" "kongair_global_cp" {
   name = "KongAir_Global"
@@ -79,42 +67,12 @@ resource "konnect_team_role" "kong_air_internal_cp_viewer" {
   role_name        = "Viewer"
   team_id          = konnect_team.kong_air_internal_viewers.id
 }
-# External Developers Team
-# The external developers team is responsible for managing the external control plane configuration
-
-resource "konnect_team" "kong_air_external_devs" {
-  description = "Allow managing the external control plane configuration."
-  name        = "Kong Air External Developers"
-}
-
-resource "konnect_team_role" "kong_air_external_cp_admin" {
-  entity_id        = konnect_gateway_control_plane.kongair_external_cp.id
-  entity_region    = "us"
-  entity_type_name = "Control Planes"
-  role_name        = "Admin"
-  team_id          = konnect_team.kong_air_external_devs.id
-}
-# External Viewers Team
-# The external viewers team is responsible for monitoring the internal control plane configuration
-
-resource "konnect_team" "kong_air_external_viewers" {
-  description = "Allow read-only access to all entities in the external control plane"
-  name        = "Kong Air External Viewers"
-}
-
-resource "konnect_team_role" "kong_air_external_cp_viewer" {
-  entity_id        = konnect_gateway_control_plane.kongair_external_cp.id
-  entity_region    = "us"
-  entity_type_name = "Control Planes"
-  role_name        = "Viewer"
-  team_id          = konnect_team.kong_air_external_viewers.id
-}
 # Platform Admins Team
 # The platform admins team is responsible for managing all entities in the global,
-# internal and external control planes.
+# internal control planes.
 
 resource "konnect_team" "platform_admins" {
-  description = "Allow managing entities in the global, internal and external control planes"
+  description = "Allow managing entities in the global, internal control planes"
   name        = "Platform Admins"
 }
 
@@ -134,19 +92,12 @@ resource "konnect_team_role" "platform_admins_internal_cp_admin" {
   team_id          = konnect_team.platform_admins.id
 }
 
-resource "konnect_team_role" "platform_admins_external_cp_admin" {
-  entity_id        = konnect_gateway_control_plane.kongair_external_cp.id
-  entity_region    = "us"
-  entity_type_name = "Control Planes"
-  role_name        = "Admin"
-  team_id          = konnect_team.platform_admins.id
-}
 # Platform Viewers Team
 # The platform viewers team is responsible for monitoring all entities in the global,
-# internal and external control planes.
+# internal control planes.
 
 resource "konnect_team" "platform_viewers" {
-  description = "Allow read-only access to all entities in the global, internal and external control planes"
+  description = "Allow read-only access to all entities in the global, internal control planes"
   name        = "Platform Viewers"
 }
 
@@ -160,14 +111,6 @@ resource "konnect_team_role" "platform_viewers_global_cp_viewer" {
 
 resource "konnect_team_role" "platform_viewers_internal_cp_viewer" {
   entity_id        = konnect_gateway_control_plane.kongair_internal_cp.id
-  entity_region    = "us"
-  entity_type_name = "Control Planes"
-  role_name        = "Viewer"
-  team_id          = konnect_team.platform_viewers.id
-}
-
-resource "konnect_team_role" "platform_viewers_external_cp_viewer" {
-  entity_id        = konnect_gateway_control_plane.kongair_external_cp.id
   entity_region    = "us"
   entity_type_name = "Control Planes"
   role_name        = "Viewer"
